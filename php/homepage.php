b36b2b0fa804535e04fbd01fcbc01a47be71b55e
saaketh <jella.saaketh@s.amity.edu>
2022-07-17 12:39:34 +0530

    navigation links

--- php/homepage.php
+++ php/homepage.php
@@ -6,8 +6,57 @@
         <title>Homepage</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="../css/homepage.css">
     </head>
     <body>
+        <div class="container">
+            <div class="nav-links">
+                <div class="icons-description">
+                    <img src="../logos/twitter-logo.svg" alt="logo" height="25px">
+                </div>
+                <div class="icons-description">
+                    <a href="">
+                        <img src="../logos/home.svg" alt="home" height="28px">
+                        <p>Home</p>
+                    </a>
+                </div>
+                <div class="icons-description">
+                <a href="">
+                        <img src="../logos/explore.png" alt="explore" height="28px">
+                        <p>Explore</p>
+                    </a>
+                </div>
+                <div class="icons-description">
+                <a href="">
+                        <img src="../logos/notification.svg" alt="notification" height="28px">
+                        <p>Notifications</p>
+                    </a>
+                </div>
+                <div class="icons-description">
+                <a href="">
+                        <img src="../logos/messages.svg" alt="messages" height="28px">
+                        <p>Messages</p>
+                    </a>
+                </div>
+                <div class="icons-description">
+                <a href="">
+                        <img src="../logos/profile.svg" alt="profile" height="28px">
+                        <p>Profile</p>
+                    </a>
+                </div>
+                <div class="icons-description">
+                <a href="">
+                        <img src="../logos/more.svg" alt="more" height="28px">
+                        <p>More</p>
+                    </a>
+                </div>
+            </div>
+            <div class="main-news-feed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A delectus soluta nobis voluptatum mollitia fugiat est sapiente officiis molestias libero voluptatibus esse, nesciunt, rerum incidunt cum laboriosam quibusdam voluptates architecto.
+            </div>
+            <div class="trending">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A delectus soluta nobis voluptatum mollitia fugiat est sapiente officiis molestias libero voluptatibus esse, nesciunt, rerum incidunt cum laboriosam quibusdam voluptates architecto.
+            </div>
+        </div>
     </body>
 </html>
