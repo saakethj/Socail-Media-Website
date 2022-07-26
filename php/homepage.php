0b8897b4d96cef3281c3d7e03637d6a1a8127283
saaketh <jella.saaketh@s.amity.edu>
2022-07-26 10:22:13 +0530

    newsfeed

--- php/homepage.php
+++ php/homepage.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Homepage</title>
+        <title>Home</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../css/homepage.css">
@@ -18,45 +18,61 @@
     <body>
         <!-- sidebar starts -->
     <div class="sidebar">
-      <i class="fab fa-twitter"></i>
+      <!-- <i class="fab fa-twitter"></i> -->
+      <div class="sidebarOption">
+        <img src="../images/logo.png" alt="sdsa" class="logo" height="30px" style="padding-left: 20px;">
+      </div>
+
+      <a href="" class="links">
       <div class="sidebarOption active">
         <span class="material-icons"> home </span>
         <h2>Home</h2>
       </div>
+      </a>
 
+      <a href="" class="links">
       <div class="sidebarOption">
         <span class="material-icons"> search </span>
-        <h2>Explore</h2>
+        <h2>Seek</h2>
       </div>
+      </a>
 
+      <a href="" class="links">
       <div class="sidebarOption">
         <span class="material-icons"> notifications_none </span>
         <h2>Notifications</h2>
       </div>
+      </a>
 
+      <a href="" class="links">
       <div class="sidebarOption">
         <span class="material-icons"> mail_outline </span>
-        <h2>Messages</h2>
+        <h2>Chats</h2>
       </div>
-
+      </a>
+      
+      <a href="" class="links">
       <div class="sidebarOption">
         <span class="material-icons"> bookmark_border </span>
-        <h2>Bookmarks</h2>
+        <h2>Saved</h2>
       </div>
+      </a>
 
+      <a href="" class="links">
       <div class="sidebarOption">
         <span class="material-icons"> perm_identity </span>
-        <h2>Profile</h2>
+        <h2>Account</h2>
       </div>
+      </a>
 
-      <button class="sidebar__tweet">Tweet</button>
+      <button class="sidebar__tweet">Post</button>
     </div>
     <!-- sidebar ends -->
 
     <!-- feed starts -->
     <div class="feed">
       <div class="feed__header">
-        <h2>Home</h2>
+        <h2>News Feed</h2>
       </div>
 
       <!-- tweetbox starts -->
@@ -67,9 +83,9 @@
               src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
               alt=""
             />
-            <input type="text" placeholder="What's happening?" />
+            <input type="text" placeholder="Post anything :)" />
           </div>
-          <button class="tweetBox__tweetButton">Tweet</button>
+          <button class="tweetBox__tweetButton">Post</button>
         </form>
       </div>
       <!-- tweetbox ends -->
@@ -87,18 +103,18 @@
           <div class="post__header">
             <div class="post__headerText">
               <h3>
-                Somanath Goudar
+                JellaSaaketh
                 <span class="post__headerSpecial"
-                  ><span class="material-icons post__badge"> verified </span>@somanathg</span
+                  ><span class="material-icons post__badge"> verified </span>@saakethJ</span
                 >
               </h3>
             </div>
             <div class="post__headerDescription">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Neural networks, also known as artificial neural networks (ANNs) or simulated neural networks (SNNs), are a subset of machine learning and are at the heart of deep learning algorithms. Their name and structure are inspired by the human brain, mimicking the way that biological neurons signal to one another.</p>
             </div>
           </div>
           <img
-            src="https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03.jpg"
+            src="../images/feed photos/ann.png"
             alt=""
           />
           <div class="post__footer">
@@ -123,18 +139,20 @@
           <div class="post__header">
             <div class="post__headerText">
               <h3>
-                Somanath Goudar
+                DavidJohnson
                 <span class="post__headerSpecial"
-                  ><span class="material-icons post__badge"> verified </span>@somanathg</span
+                  ><span class="material-icons post__badge"> verified </span>@David_2612</span
                 >
               </h3>
             </div>
             <div class="post__headerDescription">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>The Avengers, American comic strip superhero team whose frequently
+              changing roster often included some of the most popular characters
+              in the Marvel Comics universe. </p>
             </div>
           </div>
           <img
-            src="https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03.jpg"
+            src="https://cdn.britannica.com/60/182360-050-CD8878D6/Avengers-Age-of-Ultron-Joss-Whedon.jpg"
             alt=""
           />
           <div class="post__footer">
@@ -145,6 +163,41 @@
         </div>
       </div>
       <!-- post ends -->
+
+      <div class="post">
+        <div class="post__avatar">
+          <img
+            src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
+            alt=""
+          />
+        </div>
+
+        <div class="post__body">
+          <div class="post__header">
+            <div class="post__headerText">
+              <h3>
+                PeterHughie
+                <span class="post__headerSpecial"
+                  ><span class="material-icons post__badge"> verified </span>@Peter_2022</span
+                >
+              </h3>
+            </div>
+            <div class="post__headerDescription">
+              <p>Keanu Reeves told Extra that he would love to play an older, wisened Batman someday: “It’s always been a dream, but Pattinson’s Batman right now. He’s doing awesome, but maybe down the road when they need an older Batman.”
+</p>
+            </div>
+          </div>
+          <!-- <img
+            src="https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03.jpg"
+            alt=""
+          /> -->
+          <div class="post__footer">
+            <span class="material-icons"> repeat </span>
+            <span class="material-icons"> favorite_border </span>
+            <span class="material-icons"> publish </span>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- feed ends -->
 
@@ -152,7 +205,7 @@
     <div class="widgets">
       <div class="widgets__input">
         <span class="material-icons widgets__searchIcon"> search </span>
-        <input type="text" placeholder="Search Twitter" />
+        <input type="text" placeholder="Search" />
       </div>
 
       <div class="widgets__widgetContainer">
